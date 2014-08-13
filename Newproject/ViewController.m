0a766f997e2d187ff3f764d15de186dba9571273
Riagms <riyariya44@gmail.com>
2014-08-13 14:54:06 +0530

    cmmtd

--- Newproject/ViewController.m
+++ Newproject/ViewController.m
@@ -118,7 +118,7 @@
                    
                    "<soap:Body>\n"
                    
-                   "<Loginselect xmlns=\"http://ios.kontract360.com/\">\n"
+                   "<Loginselect xmlns=\"http://test.kontract360.com/\">\n"
                    "<UserName>%@</UserName>\n"
                    "<Password>%@</Password>\n"
                    "<LTime>%@</LTime>\n"
@@ -131,8 +131,8 @@
     NSLog(@"soapmsg%@",soapMessage);
     
     
-   NSURL *url = [NSURL URLWithString:@"http://192.168.0.100/service.asmx"];
- // NSURL *url = [NSURL URLWithString:@"http://testusa.kontract360.com/service.asmx"];
+  // NSURL *url = [NSURL URLWithString:@"http://192.168.0.100/service.asmx"];
+  NSURL *url = [NSURL URLWithString:@"http://testusa.kontract360.com/service.asmx"];
     
     NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
     
@@ -140,7 +140,7 @@
     
     [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
     
-    [theRequest addValue: @"http://ios.kontract360.com/Loginselect" forHTTPHeaderField:@"Soapaction"];
+    [theRequest addValue: @"http://test.kontract360.com/Loginselect" forHTTPHeaderField:@"Soapaction"];
     
     [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
     [theRequest setHTTPMethod:@"POST"];
