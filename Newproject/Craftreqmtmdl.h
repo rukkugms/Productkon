e2d922171cd6c4dbc6b076b1c23dc1608ed358e2
Riagms <riyariya44@gmail.com>
2014-09-11 14:16:13 +0530

    cmmtd

---
+++ Newproject/Craftreqmtmdl.h
@@ -0,0 +1,19 @@
+//
+//  Craftreqmtmdl.h
+//  Newproject
+//
+//  Created by Riya on 9/10/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Craftreqmtmdl : NSObject
+@property(strong,nonatomic)NSString*BRreqid;
+@property(strong,nonatomic)NSString*Brentryid;
+@property(strong,nonatomic)NSString*BRvalue;
+@property(strong,nonatomic)NSString*Brdescriptn;
+
+
+
+@end
