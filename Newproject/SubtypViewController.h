e054e15463c8493ba2c591da96a5c10afd309fd3
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-09-12 12:15:13 +0530

    committed

---
+++ Newproject/SubtypViewController.h
@@ -0,0 +1,14 @@
+//
+//  SubtypViewController.h
+//  Newproject
+//
+//  Created by GMSIndia 2 on 12/09/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubtypViewController : UIViewController
+-(IBAction)closesubtype:(id)sender;
+
+@end
