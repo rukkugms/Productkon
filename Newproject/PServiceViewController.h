0c74a056fae3db93e7a57085d7541e46253fce2f
Riagms <riyariya44@gmail.com>
2014-09-22 11:34:12 +0530

    cmmtd

--- Newproject/PServiceViewController.h
+++ Newproject/PServiceViewController.h
@@ -8,6 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PServiceViewController : UIViewController
+@interface PServiceViewController : UIViewController{
+    BOOL recordResults;
+    NSString*servicestrg;
+}
+@property (strong, nonatomic) IBOutlet UITableView *servicetable;
+@property (strong, nonatomic) IBOutlet UIView *titleview;
+@property (strong, nonatomic)NSMutableDictionary*servicedict;
+@property (strong, nonatomic)NSMutableArray*servicearray;
+
+
+/* xmlparser*/
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
+@property (strong, nonatomic) IBOutlet UITableViewCell *servicecell;
+@property (strong, nonatomic) IBOutlet UIButton *addwrkbtnlbl;
+@property (strong, nonatomic) IBOutlet UILabel *servicelbl;
+- (IBAction)wrkbtn:(id)sender;
+
+- (IBAction)clsebtn:(id)sender;
 
 @end
