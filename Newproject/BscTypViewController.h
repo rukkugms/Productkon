9765deb6ac0fc934305fe26d1a8a7e8ca31cf33f
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-09-26 09:49:29 +0530

    committed

---
+++ Newproject/BscTypViewController.h
@@ -0,0 +1,13 @@
+//
+//  BscTypViewController.h
+//  Newproject
+//
+//  Created by GMSIndia 2 on 26/09/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BscTypViewController : UIViewController
+
+@end
