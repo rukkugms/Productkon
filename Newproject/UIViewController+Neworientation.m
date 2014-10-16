28a4ad2b7decedc26392d26c12c3315d7c48b6fe
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-10-16 17:32:50 +0530

    dsfdsfds

--- Newproject/UIViewController+Neworientation.m
+++ Newproject/UIViewController+Neworientation.m
@@ -9,5 +9,16 @@
 #import "UIViewController+Neworientation.h"
 
 @implementation UIViewController (Neworientation)
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return UIInterfaceOrientationIsLandscape(toInterfaceOrientation);
+}
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
+}
 
 @end
