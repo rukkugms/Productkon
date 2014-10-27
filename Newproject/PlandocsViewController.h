c7eb3cdfbfa848823064a88f7ba7b6f2259e2c2a
Riagms <riyariya44@gmail.com>
2014-10-27 17:21:09 +0530

    cmmtd

--- Newproject/PlandocsViewController.h
+++ Newproject/PlandocsViewController.h
@@ -7,18 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface PlandocsViewController : UIViewController
+#import "WebViewController.h"
+@interface PlandocsViewController : UIViewController{
+    BOOL recordResults;
+    NSString *docname;
+    UIButton *button;
+}
 @property (strong, nonatomic) IBOutlet UINavigationBar *navbar;
+@property (strong, nonatomic)WebViewController *webVCtrl;
 
 - (IBAction)clsebtn:(id)sender;
-
+@property (strong, nonatomic)NSString *planid;
 @property (strong, nonatomic) IBOutlet UITableViewCell *doccell;
 @property (strong, nonatomic) IBOutlet UILabel *docnamelbl;
 @property (strong, nonatomic) IBOutlet UIButton *viewbtn;
 
 @property (strong, nonatomic) IBOutlet UITableView *documnttable;
 @property (strong, nonatomic)NSMutableArray *documntarray;
-
+@property (strong, nonatomic)NSMutableDictionary *docmntdict;
+/* xmlparser*/
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
+- (IBAction)viewbtn:(id)sender;
 
 @end
