85c35ff24ce0a811557dca70e86b52465744f60a
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-11-06 09:52:34 +0530

    hjhkjh

--- Newproject/AppDelegate.m
+++ Newproject/AppDelegate.m
@@ -89,6 +89,7 @@ return YES;
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    [(Sessionout *)[UIApplication sharedApplication] resetIdleTimer];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
    
 }
