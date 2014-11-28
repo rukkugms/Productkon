96f40b23f3d416aec503cd9b3d35edd59468bf7a
Riagms <riyariya44@gmail.com>
2014-11-28 10:18:29 +0530

    cmmtd

---
+++ Newproject/GPConusumViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPConusumViewController.h
+//  Newproject
+//
+//  Created by Riya on 11/28/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPConusumViewController : UIViewController
+
+@end
---
+++ Newproject/GPConusumViewController.m
@@ -0,0 +1,37 @@
+//
+//  GPConusumViewController.m
+//  Newproject
+//
+//  Created by Riya on 11/28/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import "GPConusumViewController.h"
+
+@interface GPConusumViewController ()
+
+@end
+
+@implementation GPConusumViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
