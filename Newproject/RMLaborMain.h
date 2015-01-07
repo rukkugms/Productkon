80d8012eb7d2f4190186e32ee5e52dd42feb50c1
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2015-01-07 15:55:07 +0530

    new model

---
+++ Newproject/RMLaborMain.h
@@ -0,0 +1,13 @@
+//
+//  RMLaborMain.h
+//  Newproject
+//
+//  Created by GMSIndia 2 on 07/01/15.
+//  Copyright (c) 2015 GMSIndia1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMLaborMain : NSObject
+
+@end
