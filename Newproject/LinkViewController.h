10af1e36c7dce4f590ff3dee9d3bb2e6e2b61528
Riya <macindia@Riya.local>
2015-01-07 15:42:37 +0530

    new file added

---
+++ Newproject/LinkViewController.h
@@ -0,0 +1,13 @@
+//
+//  LinkViewController.h
+//  Newproject
+//
+//  Created by Riya on 1/7/15.
+//  Copyright (c) 2015 GMSIndia1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinkViewController : UIViewController
+
+@end
